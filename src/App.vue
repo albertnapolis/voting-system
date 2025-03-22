7b6011775de89d8f5921bc2cb3ddcc988ac0ce26
albert napolis <hamster@alberts-Mac-mini.local>
2025-03-22 19:55:57 +0800

    feat: fix desktop view

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,22 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div class="main-container w-100 h-100">
+    <login-view />
+  </div>
 </template>
 
+<script lang="ts">
+  import { defineComponent } from 'vue';
+  import LoginView from './views/LoginView.vue';
+
+  export default defineComponent({
+    components: { LoginView },
+    setup() {
+
+      return {}
+    }
+  })
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
---
+++ src/views/LoginView.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="login-container w-100 h-100 d-flex flex-column flex-sm-row aling-items-center justify-content-center ">
+        <div class="d-flex align-items-center justify-content-center justify-content-sm-end">
+            <div class="logo">
+                <img src="../assets/Pasted image 20250322143827.png" alt="logo" class="img-fluid">
+            </div>
+        </div>
+        <div class="d-none d-sm-flex align-items-center justify-content-center">
+            <span class="divider gradient-background mx-5 rounded-1 h-25" style="padding: 1px;"></span>
+        </div>
+        <div class="login-form d-flex align-items-center">
+            <form class="w-100 mx-3 mx-sm-0">
+                <div class="mb-3">
+                  <label for="studentID" class="form-label">Student ID</label>
+                  <input class="form-control border-2 border-black rounded-1" id="studentID" placeholder="e.g 20116">
+                </div>
+                <div class="mb-3">
+                  <label for="studentPassword" class="form-label">Password</label>
+                  <input type="password" class="form-control border-2 border-black rounded-1" id="studentPassword">
+                </div>
+                <button type="submit" class="btn w-100 rounded-1 text-white shadow" style="background: #800000">Login</button>
+            </form>
+        </div>
+    </div>
+</template>
